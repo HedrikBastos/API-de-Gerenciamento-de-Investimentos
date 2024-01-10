d7f8e1c3fa824b9c248bc9a8e5571a6acaca8461
HedrikBastos <hedrikbastos@outlook.com>
2024-01-10 00:18:42 -0300

    Readme

--- src/Controller/Proprietario/Investimento/Sacar/Controller.php
+++ src/Controller/Proprietario/Investimento/Sacar/Controller.php
@@ -21,4 +21,4 @@ class Controller extends AbstractController
             'SaldoInvestimento' => $executaSacaInvestimentoService
         ]);
     }
-}
+}                                                       
